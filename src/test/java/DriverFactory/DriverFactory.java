0ad52cb7fd80cac28fdbb86fa4384f7fefd754f7
shivacs <shivagami.parthiban@gmail.com>
2025-11-20 17:03:57 -0500

    test Runner Cross browser and parallel changes

--- src/test/java/DriverFactory/DriverFactory.java
+++ src/test/java/DriverFactory/DriverFactory.java
@@ -1,5 +1,7 @@
 package DriverFactory;
 
+import java.time.Duration;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
@@ -7,27 +9,63 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import Utils.ConfigReader;
+import io.cucumber.java.After;
 
 public class DriverFactory {
 	private static final Logger logger = LogManager.getLogger(DriverFactory.class);
+	private static final ThreadLocal<WebDriver> tldriver = new ThreadLocal<>();
+	private static ThreadLocal<String> plbrowser = new ThreadLocal<>();
+
+	// ThreadLocal<>();
 	private static WebDriver driver;
+	// private static String plbrowser;
 
 	public static WebDriver getDriver() {
-		return driver;
+		return tldriver.get();
+
+	}
+
+	// private static String plbrowser;
+
+	public static void setupBrowser(String browser) {
+		plbrowser.set(browser);
+		System.out.println(browser);
+
 	}
 
 	public static void initDriver() {
-		String browserName = ConfigReader.getProperty("browser");
-		logger.info("You selected "+browserName+ " to run these tests");
+		// String browserName = ConfigReader.getProperty("browser");
+		// String browserName = System.getProperty("browser");
+		// String browserName = ScenarioContext.get("browser");
 
+		String browserName = plbrowser.get();
+		System.out.println(browserName);
+		logger.info("You selected " + browserName + " to run these tests");
+		// WebDriver instance = new ChromeDriver();
 		if (browserName.trim().equalsIgnoreCase("chrome")) {
-			driver = new ChromeDriver();
+			tldriver.set(new ChromeDriver());
+
 		} else if (browserName.trim().equalsIgnoreCase("firefox")) {
-			driver = new FirefoxDriver();
+			tldriver.set(new FirefoxDriver());
+
 		} else if (browserName.trim().equalsIgnoreCase("edge")) {
-			driver = new EdgeDriver();
+			tldriver.set(new EdgeDriver());
+
+		} else {
+			logger.info("You selected wrong browser: " + browserName);
+			return;
 		}
+		driver = tldriver.get();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.manage().window().maximize();
+
 	}
+
+	@After(order = 0)
+	public void close() {
+
+		tldriver.remove();
+
+	}
+
 }
